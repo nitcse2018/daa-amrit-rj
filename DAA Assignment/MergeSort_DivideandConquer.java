1b35ce062eacdd4ff93a6809e1fbad98c63c7419
amrit-rj <62424128+amrit-rj@users.noreply.github.com>
2020-04-17 12:31:34 +0530

    Divide and Conquer

--- DAA Assignment/MergeSort_DivideandConquer.java
+++ DAA Assignment/MergeSort_DivideandConquer.java
@@ -1,25 +1,72 @@
-// Fibonacci Series using Dynamic Programming
+//Sorting of elements using Divide and Conquer algorithm
 /*Output:
-Fibonacci Series: 
-[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]*/ 
-import java.util.Arrays;
-public class Fibonacci 
+5 6 7 11 12 13*/
+public class MergeSort 
 { 
-	int[] fib(int n) 
+	void merge(int arr[], int l, int m, int r) 
 	{ 
-		int f[] = new int[n+1];
-		int i; 
-		f[0] = 0; 
-		f[1] = 1; 
-		for (i = 2; i <= n; i++)  
-			f[i] = f[i-1] + f[i-2];
-		return f; 
+		int n1 = m - l + 1; 
+		int n2 = r - m; 
+		int L[] = new int [n1]; 
+		int R[] = new int [n2]; 
+		for (int i=0; i<n1; ++i) 
+			L[i] = arr[l + i]; 
+		for (int j=0; j<n2; ++j) 
+			R[j] = arr[m + 1+ j]; 
+		int i = 0, j = 0; 
+		int k = l; 
+		while (i < n1 && j < n2) 
+		{ 
+			if (L[i] <= R[j]) 
+			{ 
+				arr[k] = L[i]; 
+				i++; 
+			} 
+			else
+			{ 
+				arr[k] = R[j]; 
+				j++; 
+			} 
+			k++; 
+		while (i < n1) 
+		{ 
+			arr[k] = L[i]; 
+			i++; 
+			k++; 
+		while (j < n2) 
+		{ 
+			arr[k] = R[j]; 
+			j++; 
+			k++; 
+		} 
 	} 
-	
-	public static void main (String args[]) 
+	void sort(int arr[], int l, int r) 
 	{ 
-		Fibonacci ob=new Fibonacci();
-		int n = 10; 
-		System.out.println("Fibonacci Series: \n"+Arrays.toString(ob.fib(n))); 
+		if (l < r) 
+			int m = (l+r)/2; 
+			sort(arr, l, m); 
+			sort(arr , m+1, r); 
+			merge(arr, l, m, r); 
+		} 
 	} 
-} 
+	static void printArray(int arr[]) 
+	{ 
+		int n = arr.length; 
+		for (int i=0; i<n; ++i) 
+			System.out.print(arr[i] + " "); 
+		System.out.println(); 
+	} 
+	public static void main(String args[]) 
+	{ 
+		int arr[] = {12, 11, 13, 5, 6, 7}; 
+
+		System.out.println("Given Array"); 
+		printArray(arr); 
+
+		MergeSort ob = new MergeSort(); 
+		ob.sort(arr, 0, arr.length-1); 
+
+		System.out.println("\nSorted array"); 
+		printArray(arr); 
+	} 
+}
